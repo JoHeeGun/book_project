df734df448de3381ce459161c641702290669f4a
JoHeeGun <whgmlrjs123@naver.com>
2022-03-16 15:23:06 +0900

    도서 검색 옵션 영어 -> 한글로 변경

--- frontend/src/components/book/BookSearch.vue
+++ frontend/src/components/book/BookSearch.vue
@@ -18,6 +18,36 @@
           {{ item }}
         </option>
       </select>
+
+      <!-- <el-select
+      v-model="sort"
+      placeholder="정확도순"
+      size="large"
+      style="margin-left: 2%"
+    >
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+
+    <el-select
+      v-model="target"
+      placeholder="제목"
+      size="large"
+      style="margin-left: 2%"
+    >
+      <el-option
+        v-for="item in options2"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select> -->
     </div>
     <button class="btn btn-outline-primary" @click="apply">검색</button>
   </div>
@@ -47,6 +77,38 @@ export default {
       ],
     };
   },
+  // data() {
+  //   return {
+  //     options: [
+  //       {
+  //         value: 'accuracy',
+  //         label: '정확도순',
+  //       },
+  //       {
+  //         value: 'latest',
+  //         label: '발간일순',
+  //       },
+  //     ],
+  //     options2: [
+  //       {
+  //         value: 'title',
+  //         label: '제목',
+  //       },
+  //       {
+  //         value: 'publisher',
+  //         label: '출판사',
+  //       },
+  //       {
+  //         value: 'person',
+  //         label: '저자',
+  //       },
+  //     ],
+
+  //     sort: '',
+  //     target: '',
+  //     title: '',
+  //   };
+  // },
   methods: {
     async apply() {
       this.$store.dispatch('book/searchBooks', {
