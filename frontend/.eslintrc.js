88a2af534b5a3b661e5f9aebb42f6020311c5cd9
JoHeeGun <whgmlrjs123@naver.com>
2022-12-11 11:50:39 +0900

    eslint 수정

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,19 +6,19 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": "off",
-    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-    "prettier/prettier": ['error', {
-      singleQuote: true,
-      semi: true,
-      useTabs: false,
-      tabWidth: 2,
-      trailingComma: 'all',
-      printWidth: 80,
-      bracketSpacing: true,
-      arrowParens: 'avoid',
-      endOfLine:'auto',
-    }]
+    "prettier/prettier": ['error', 
+        {
+          singleQuote: true,
+          semi: true,
+          useTabs: false,
+          tabWidth: 2,
+          trailingComma: 'all',
+          printWidth: 80,
+          bracketSpacing: true,
+          arrowParens: 'avoid',
+          endOfLine:'auto',
+        },
+    ]
   },
   parserOptions: {
     parser: "babel-eslint"
